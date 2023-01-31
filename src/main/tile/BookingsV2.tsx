869af52cf8703ed380f60280c31d71603649abd3
Prasad Edlabadkar <prasad.edlabadkar@outlook.com>
2023-01-31 10:39:31 +0530

    Loading message for customers

--- src/main/tile/BookingsV2.tsx
+++ src/main/tile/BookingsV2.tsx
@@ -1,11 +1,12 @@
 import { SetStateAction, useEffect, useState } from "react";
-import { Button, Card, Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Row, Spinner } from "react-bootstrap";
 import callAPI, { currencyFormatter, formatDate } from "./Utility";
 import * as Icon from 'react-bootstrap-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpa } from "@fortawesome/free-solid-svg-icons";
 
 export default function BookingsV2({ token, setToken }: { token: string, setToken: any }) {
+    const [loading, setLoading] = useState(true);
     const [billingData, setBillingData] = useState([
         {
             "id": 0,
@@ -203,10 +204,15 @@ export default function BookingsV2({ token, setToken }: { token: string, setToke
     }, []);
 
     const loadData = () => {
+        setLoading(true);
         const apiURL = `https://api.dingg.app/api/v1/vendor/bills?web=true&page=1&limit=1000&start=${formatDate(new Date())}&end=${formatDate(new Date())}&term=&is_product_only=`;
         //const apiURL = `https://api.dingg.app/api/v1/vendor/bills?web=true&page=1&limit=1000&start=2023-01-22&end=2023-01-22&term=&is_product_only=`
         callAPI(apiURL, token, setToken, (data: any) => {
             let counter = 0;
+            if(data.data.length === 0) {
+                setLoading(false);
+                setBillingData([]);
+            } else {
             for (var billIndex in data.data) {
                 const bill = data.data[billIndex];
                 const billURL = `https://api.dingg.app/api/v1//bill?bill_id=${bill.bill_payments[0].bill_id}`;
@@ -223,15 +229,18 @@ export default function BookingsV2({ token, setToken }: { token: string, setToke
                     bill.payments.total = billData.data.total;
                     counter++;
                     if (counter === data.data.length) {
+                        setLoading(false);
                         identifyMembers(data);
                     }
                 });
             }
+        }
 
         });
     }
 
     const identifyMembers = (data: { data: SetStateAction<{ id: number; selected_date: string; bill_number: string; total: number; paid: number; payment_status: string; status: boolean; cancel_reason: string; net: number; tax: number; roundoff: number; user: { id: number; fname: string; lname: string; display_name: string; mobile: string; is_member: boolean; }; products: { employee_id: number; price: number; qty: number; discount: number; tax: number; total: number; net: number; paid: number; redeem: number; discount_id: string; discount_type: string; emp_share_on_redeem: number; p_modes: { amount: number; payment_mode: number; }[]; product_lot_id: string; tax_percent: number; tax_1_percent: number; tax_2_percent: number; product: { id: string; name: string; sac_code: number; }; employee: { id: number; name: string; }; product_lot: null; }[]; services: { id: number; price: number; qty: number; discount: number; tax: number; total: number; net: number; paid: number; redeem: number; discount_id: string; discount_type: string; emp_share_on_redeem: number; p_modes: { amount: number; payment_mode: number; }[]; tax_percent: number; tax_1_percent: number; tax_2_percent: number; employee: { id: number; name: string; }; vendor_service: { id: number; service: string; service_time: string; sub_category: { sac_code: string; }; }; bill_service_splits: never[]; }[]; memberships: { id: number; price: number; qty: number; discount: number; tax: number; total: number; net: number; paid: number; redeem: number; discount_id: string; discount_type: string; emp_share_on_redeem: number; p_modes: { amount: number; payment_mode: number; }[]; tax_percent: number; tax_1_percent: number; tax_2_percent: number; employee: { id: number; name: string; }; membership: { id: number; membership_type: { id: number; type: string; }; }; bill_service_splits: never[]; }; packages: { id: number; employee_id: number; price: number; qty: number; discount: number; tax: number; total: number; net: number; paid: number; redeem: number; discount_id: null; discount_type: null; emp_share_on_redeem: number; p_modes: { amount: number; payment_mode: number; }[]; tax_percent: number; tax_1_percent: number; tax_2_percent: number; package: { id: number; package_type: { id: number; package_name: string; }; }; employee: { id: number; name: string; }; }; payments: { price: number; discount: number; tax: number; total: number; }; bill_payments: { id: number; bill_id: number; payment_date: string; payment_mode: number; amount: number; redemption: boolean; note: string; vendor_location_id: string; createdAt: string; updatedAt: string; }[]; }[]>; }) => {
+        
         const memberURL = `https://api.dingg.app/api/v1//vendor/customer_list?page=1&limit=500&amount_start=0&membership_type=0&amount_start=0&is_multi_location=false`;
         callAPI(memberURL, token, setToken, (memberData: any) => {
             for (var billIndex in data.data) {
@@ -243,6 +252,7 @@ export default function BookingsV2({ token, setToken }: { token: string, setToke
                     data.data[billIndex].user.is_member = false;
                 }
             }
+            setLoading(false);
             setBillingData(data.data);
         });
     }
@@ -265,10 +275,13 @@ export default function BookingsV2({ token, setToken }: { token: string, setToke
             </Row>
             <Row>
                 {
+                    billingData.length === 0? <Col xl={4} xs={12} className="gy-4"><Card className="shadow" bg="primary" text="light"><Card.Body><h2>{loading?'Fetching customer list':'No customers yet.'}</h2></Card.Body></Card></Col>:
                     billingData.map((booking, index) => {
                         return (
                             <Col xl={4} xs={12} className="gy-4" key={"booking" + index}>
                                 <Card className="shadow" bg={booking.status ? 'success' : 'danger'} text="light" >
+                                    {
+                                        loading ? <Card.Body><Spinner animation="grow" /></Card.Body> :
                                     <Card.Body>
                                         <div>
                                             <h3>{booking.user.is_member ? <Icon.StarFill style={{ marginTop: -4, paddingRight: 4 }} color="gold" /> : ''}{booking.user.display_name || `${booking.user.fname || ""} ${booking.user.lname || ""}`.trim()} ({currencyFormatter.format(booking.payments.total)})</h3>
@@ -298,14 +311,16 @@ export default function BookingsV2({ token, setToken }: { token: string, setToke
                                             </ul>
                                         </div>
                                     </Card.Body>
+                    }
                                     {
+                                        !loading?(
                                         booking.status ? <Card.Footer className="w-100">
                                             <div className="text-start d-inline small">Without discount: {currencyFormatter.format(booking.payments.price)}</div>
                                             <div className="text-end d-inline small float-end">Discount: {currencyFormatter.format(booking.payments.discount)} ({Math.round(booking.payments.discount * 100 / booking.payments.price)}%)</div>
                                         </Card.Footer> :
                                             <Card.Footer className="w-100">
                                                 <div className="text-start d-inline small">Cancellation Reason: {booking.cancel_reason}</div>
-                                            </Card.Footer>
+                                            </Card.Footer>):''
                                     }
 
                                 </Card>
