04735ce1954e964548479f0a9d48d117487e4a99
Prasad Edlabadkar <prasad.edlabadkar@outlook.com>
2023-04-06 21:32:24 +0530

    New add expense feature

--- src/main/tile/Expenses.tsx
+++ src/main/tile/Expenses.tsx
@@ -1,12 +1,70 @@
-import { useEffect, useState } from "react";
-import { Accordion, Button, Card, Spinner } from "react-bootstrap";
-import callAPI, { currencyFormatter, formatDate, formatDisplayDate, getStartOfFinanceMonth, getStartOfFinanceMonthDate } from "./Utility";
+import { useEffect, useRef, useState } from "react";
+import { Accordion, Button, ButtonGroup, Card, Col, Form, Offcanvas, Row, Spinner } from "react-bootstrap";
+import callAPI, { callPOSTAPI, currencyFormatter, formatDate, formatDisplayDate, getStartOfFinanceMonth, getStartOfFinanceMonthDate } from "./Utility";
 import * as Icon from 'react-bootstrap-icons';
 
 export default function Expenses({ token, setToken }: { token: string, setToken: any }) {
     const [expenseData, setExpenseData] = useState({});
     const [loading, setLoading] = useState(true);
     const [total, setTotal] = useState(-1);
+    const [show, setShow] = useState(false);
+    const [activeButtonIndex, setActiveButtonIndex] = useState(0);
+    const [expenseTypes, setExpenseTypes] = useState([]);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const expenseType = useRef<HTMLSelectElement>(null);
+    const expenseDate = useRef<HTMLInputElement>(null);
+    const givenTo = useRef<HTMLInputElement>(null);
+    const amount = useRef<HTMLInputElement>(null);
+    const description = useRef<HTMLTextAreaElement>(null);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const createExpense = (e: any) => {
+        e.preventDefault();
+        if (expenseType.current?.value === '' || expenseDate.current?.value === '' || givenTo.current?.value === '' || amount.current?.value === '' || description.current?.value === '') {
+            setErrorMessage("Please fill all the fields");
+            return;
+        }
+        const data = {
+            "date": expenseDate.current?.value,
+            "type": expenseType.current?.value,
+            "mode": activeButtonIndex === 0 ? "3" : "1",
+            "given_to": givenTo.current?.value,
+            "amount": Number.parseFloat(`${amount.current?.value}`),
+            "desc": description.current?.value,
+            "vendor_account_id": activeButtonIndex === 0 ? "712" : "2348",
+        }
+
+        callPOSTAPI("https://api.dingg.app/api/v1/vendor/expense", data, token, setToken, (response: any) => {
+            if (response.success) {
+                setErrorMessage('');
+                handleClose();
+            } else {
+                setErrorMessage(response.message);
+            }
+        })
+
+        // const requestMetadata = {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': token
+        //     },
+        //     body: JSON.stringify(data)
+        // };
+        // fetch("https://api.dingg.app/api/v1/vendor/expense", requestMetadata)
+        //     .then(res => res.json())
+        //     .then(response => {
+        //         if(response.success) {
+        //             setErrorMessage('');
+        //             handleClose();
+        //         } else {
+        //             setErrorMessage(response.message);
+        //         }
+        //     });
+    }
     // eslint-disable-next-line no-sequences
     const groupBy = (x: any[], f: { (v: string): any; (arg0: any, arg1: any, arg2: any): string | number; }) => x.reduce((a, b, i) => ((a[f(b, i, x)] ||= []).push(b), a), {});
 
@@ -25,6 +83,11 @@ export default function Expenses({ token, setToken }: { token: string, setToken:
             setTotal(total);
             setLoading(false);
         });
+
+        const expenseTypesURL = `https://api.dingg.app/api/v1/vendor/expense/type`;
+        callAPI(expenseTypesURL, token, setToken, (data: any) => {
+            setExpenseTypes(data.data.map((v: { value: string; name: string; }) => { return { id: v.value, name: v.name } }));
+        });
     }
 
     const refresh = () => {
@@ -40,8 +103,80 @@ export default function Expenses({ token, setToken }: { token: string, setToken:
                         <div className="position-relative">
                             <h2>Monthly Expenses <p className="small mb-1">{formatDisplayDate(getStartOfFinanceMonthDate(new Date()))} to {formatDisplayDate(new Date())}</p><p className="small mb-0 text-white-50">Total: {currencyFormatter.format(total)}</p></h2>
                             <div className="position-absolute top-0 end-0" style={{ marginTop: -6 }}>
+                                {/* <Button variant="danger" className="text-light" size="sm" onClick={() => handleShow()}>Add New Expense</Button> */}
+                                <Button variant="indigo" className="text-light" size="lg" onClick={() => handleShow()}><Icon.PlusLg /></Button>
                                 <Button variant="indigo" className="text-light" size="lg" onClick={() => refresh()}><Icon.ArrowClockwise /></Button>
                             </div>
+                            <Offcanvas show={show} className="h-auto bg-dark text-white" placement="bottom" backdrop={true} scroll={false} keyboard={false} id="offcanvasBottom" onHide={handleClose}>
+                                <Offcanvas.Header closeButton closeVariant="white"><h5>Add New Expense</h5></Offcanvas.Header>
+                                <Offcanvas.Body className="pt-0">
+                                    <Form className="mt-0" onSubmit={createExpense}>
+                                        <Row className="align-items-center mb-2">
+                                            <Col xs={12}>
+                                                <Form.Group>
+                                                    <Form.Label className="mb-1">Expense Type</Form.Label>
+                                                    <Form.Select ref={expenseType}>
+                                                        {expenseTypes.map((v: { id: string; name: string; }) => <option value={v.id}>{v.name}</option>)}
+                                                    </Form.Select>
+                                                </Form.Group>
+                                            </Col>
+                                        </Row>
+                                        <Row className="align-items-center mb-1">
+                                            <Col xs={6}>
+                                                <Form.Group>
+                                                    <Form.Label className="mb-1">Expense Date</Form.Label>
+                                                    <Form.Control size="sm" type="date" placeholder="Date" ref={expenseDate} />
+                                                </Form.Group>
+                                            </Col>
+                                            <Col xs={6}>
+                                                <Form.Group>
+                                                    <Form.Label className="mb-1">Given To</Form.Label>
+                                                    <Form.Control size="sm" type="text" placeholder="Name" ref={givenTo} />
+                                                </Form.Group>
+                                            </Col>
+                                        </Row>
+                                        <Row className="align-items-end mb-1">
+                                            <Col xs={6}>
+                                                <Form.Group>
+                                                    <Form.Label className="mb-1">Amount</Form.Label>
+                                                    <Form.Control size="sm" type="number" step={0.01} placeholder="Amount" ref={amount} />
+                                                </Form.Group>
+                                            </Col>
+                                            <Col xs={6}>
+                                                <Form.Group>
+                                                    <ButtonGroup size="sm">
+                                                        <Button variant={activeButtonIndex === 0 ? "primary" : "light"} onClick={() => setActiveButtonIndex(0)}>Bank Transfer</Button>
+                                                        <Button variant={activeButtonIndex === 1 ? "primary" : "light"} onClick={() => setActiveButtonIndex(1)}>Cash</Button>
+                                                    </ButtonGroup>
+                                                </Form.Group>
+                                            </Col>
+                                        </Row>
+
+                                        <Row className="align-items-center mb-2">
+                                            <Col xs={12}>
+                                                <Form.Group>
+                                                    <Form.Label className="mb-1">Given For</Form.Label>
+                                                    <Form.Control as="textarea" rows={2} size="sm" ref={description} />
+                                                </Form.Group>
+                                            </Col>
+                                        </Row>
+                                        <Row className="align-items-center">
+                                            <Col xs={12}>
+                                                <Button variant="success" className="text-light" type="submit">Save</Button>
+                                            </Col>
+                                        </Row>
+                                        {errorMessage !== '' && <Row className="align-items-center mb-2">
+                                            <Col xs={12}>
+                                                <Form.Group>
+                                                    <Form.Label className="mb-1 text-danger">{errorMessage}</Form.Label>
+                                                </Form.Group>
+                                            </Col>
+                                        </Row>}
+
+                                    </Form>
+                                </Offcanvas.Body>
+                            </Offcanvas>
+
                         </div>
 
                         {
--- src/main/tile/Utility.tsx
+++ src/main/tile/Utility.tsx
@@ -1,47 +1,63 @@
 import moment from 'moment';
 export default function callAPI(url: string, token: string, setToken: any, cb: any) {
-    const requestMetadata = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': token
-        }
-    };
-    fetch(url, requestMetadata)
-        .then(res => res.status === 401? setToken(null):res.json())
-        .then(data => {
-            cb(data);
-        });
+  const requestMetadata = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token
+    }
+  };
+  fetch(url, requestMetadata)
+    .then(res => res.status === 401 ? setToken(null) : res.json())
+    .then(data => {
+      cb(data);
+    });
+}
+
+export function callPOSTAPI(url: string, data: object, token: string, setToken: any, cb: any) {
+  const requestMetadata = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token
+    },
+    body: JSON.stringify(data)
+  };
+  fetch(url, requestMetadata)
+    .then(res => res.status === 401 ? setToken(null) : res.json())
+    .then(data => {
+      cb(data);
+    });
 }
 
 function setCookie(cname: string, cvalue: string, exdays: number) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-  
-  function getCookie(cname: string) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname: string) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
   }
+  return "";
+}
 
 export function padTo2Digits(num: number) {
-    return num.toString().padStart(2, '0');
+  return num.toString().padStart(2, '0');
 }
 
 export function formatDate(dt: Date): string {
-    return [dt.getFullYear(), padTo2Digits(dt.getMonth() + 1), padTo2Digits(dt.getDate())].join('-');
+  return [dt.getFullYear(), padTo2Digits(dt.getMonth() + 1), padTo2Digits(dt.getDate())].join('-');
 }
 
 export function getStartOfMonth(dt: Date): string {
@@ -75,7 +91,7 @@ export function formatTime(dt: Date): string {
 }
 
 export const currencyFormatter = Intl.NumberFormat('en-in', { style: "currency", currency: "INR", maximumFractionDigits: 0 });
-  
+
 export function getFirstDayOfWeek(d: Date) {
   const date = new Date(d);
   const day = date.getDay();
@@ -92,17 +108,17 @@ export function addDays(dt: Date, days: number): Date {
 
 export function formatDisplayDate(d: Date) {
   return d.toLocaleDateString('en-GB', {
-      day: 'numeric', month: 'short', year: 'numeric'
+    day: 'numeric', month: 'short', year: 'numeric'
   }).replace(/ /g, '-');
 }
 
 export function formatWeekDay(d: Date) {
   return d.toLocaleDateString('en-GB', { weekday: 'long' })
 }
-export {setCookie, getCookie};
+export { setCookie, getCookie };
 
 export function formatMinutes(d: number) {
   return moment.duration(d, "minute").humanize()
 }
 
-export function nth(n: number){return["st","nd","rd"][(((n<0?-n:n)+90)%100-10)%10-1]||"th"}
+export function nth(n: number) { return ["st", "nd", "rd"][(((n < 0 ? -n : n) + 90) % 100 - 10) % 10 - 1] || "th" }
