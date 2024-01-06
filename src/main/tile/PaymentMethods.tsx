d3f89aadc49612528edd0b2bca47975c06c1f1f9
Prasad Edlabadkar <prasad.edlabadkar@outlook.com>
2024-01-06 22:03:36 +0400

    transaction details

--- src/main/tile/PaymentMethods.tsx
+++ src/main/tile/PaymentMethods.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import {Col, OverlayTrigger, ProgressBar, Row, Tooltip } from "react-bootstrap";
+import { Col, OverlayTrigger, ProgressBar, Row, Tooltip, Offcanvas } from "react-bootstrap";
 import { currencyFormatter, formatDate, getLastMonth } from "./Utility";
 import { TokenContext } from "../../App";
 import DiwaButtonGroup from "../../components/button/DiwaButtonGroup";
@@ -10,13 +10,16 @@ export default function PaymentMethods() {
     const { callAPI } = useContext(TokenContext);
     const [reportData, setReportData] = useState({ data: [{ total: 0, "payment mode": "" }] });
     const [total, setTotal] = useState(-1);
-    const [dayReportData, setDayReportData] = useState({ data: [{ total: 0, "payment mode": "" }] });
+    const [dayReportData, setDayReportData] = useState({ data: [{ total: 0, "payment mode": "", count: 0 }] });
     const [dayTotal, setDayTotal] = useState(-1);
     const buttonState = useState(0);
     const [, setActiveButtonIndex] = buttonState;
     const [endDate, setEndDate] = useState(new Date());
     const [startDate, setStartDate] = useState(new Date(endDate.getFullYear(), endDate.getMonth(), 1));
     const [loading, setLoading] = useState(true);
+    const [show, setShow] = useState(false);
+    const [paymentTypes, setPaymentTypes] = useState([{ name: "", value: 0 }]);
+    const [todaysBills, setTodaysBills] = useState([{ user: { fname: "", lname: "" }, bill_payments: [{ payment_mode: 0, amount: 0 }] }]);
 
     useEffect(() => {
         setLoading(true);
@@ -37,6 +40,18 @@ export default function PaymentMethods() {
                 setLoading(false);
             });
         });
+
+        const paymentTypeAPIURL = "https://api.dingg.app/api/v1/payment_mode";
+        callAPI(paymentTypeAPIURL, (data: any) => {
+            setPaymentTypes(data.data);
+        });
+
+        //https://api.dingg.app/api/v1/vendor/bills?web=true&page=1&limit=1000&start=2024-01-06&end=2024-01-06&term=&is_product_only=
+        const todaysBillsURL = `https://api.dingg.app/api/v1/vendor/bills/?web=true&page=1&limit=1000&start=${formatDate(new Date())}&end=${formatDate(new Date())}&term=&is_product_only=`;
+        callAPI(todaysBillsURL, (data: any) => {
+            setTodaysBills(data.data);
+        });
+
         const calculateToday = (data: { data: string | any[]; }) => {
             const len = data.data.length;
             let sum = 0;
@@ -88,6 +103,32 @@ export default function PaymentMethods() {
         { title: getLastMonth().toLocaleDateString('en-GB', { month: 'long' }), onClick: () => setDuration('previous') }
     ]
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [selectedPaymentMode, setSelectedPaymentMode] = useState("");
+    const [bills, setBills] = useState([{ name: "", amount: 0 }]);
+
+    const getPaymentModeId = (name: string) => {
+        return paymentTypes.find((v: { name: string; }) => v.name.toLowerCase() === name.toLowerCase())?.value;
+    }
+
+    const openDetails = (mode: string) => {
+        setSelectedPaymentMode(mode);
+        const id = getPaymentModeId(mode);
+        console.log(id);
+        console.log(todaysBills);
+        const bills = todaysBills.filter((v: { bill_payments: { payment_mode: number; }[]; }) => v.bill_payments.find((v2: { payment_mode: number; }) => v2.payment_mode === id));
+        const displayBills = bills.map((v: { user: { fname: string, lname: string }, bill_payments: { payment_mode: number; amount: number; }[]; }) => {
+            return {
+                name: v.user.fname + " " + v.user.lname,
+                amount: v.bill_payments.find((v2: { payment_mode: number; }) => v2.payment_mode === id)?.amount || 0
+            }
+        });
+        setBills(displayBills);
+        console.log(displayBills);
+        handleShow();
+    }
+
     return (
 
         <DiwaCard varient="purple" loadingTracker={loading}>
@@ -96,6 +137,27 @@ export default function PaymentMethods() {
                 <h3>Payments for {startDate.toLocaleDateString('en-GB', { month: 'long' })}</h3>
                 <DiwaRefreshButton refresh={() => refresh()} />
             </div>
+            <Offcanvas show={show} className="h-auto text-color" placement="bottom" backdrop={true} scroll={false} keyboard={false} id="offcanvasBottom" onHide={handleClose}>
+                <Offcanvas.Header closeButton closeVariant="white"><h5>{getPaymentMethodName(selectedPaymentMode)} transactions</h5></Offcanvas.Header>
+                <Offcanvas.Body className="pt-0">
+                    <ul className="list-group list-group-flush">
+                        {
+
+                            bills.map((val, index) => {
+                                return (
+                                    <li className="list-group-item bg-transparent text-color border-color ps-0" key={val + 'item' + index}>
+                                        <div className="w-100 pe-2 pb-2">
+                                            <div className="text-start d-inline">{val.name}</div>
+                                            <div className="text-end d-inline float-end">{currencyFormatter.format(val.amount)}</div>
+                                        </div>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+    
+                </Offcanvas.Body>
+            </Offcanvas>
             {
                 reportData.data.map((val, index) => {
                     const targetPercentage = Math.round(val.total * 100 / total)
@@ -120,9 +182,9 @@ export default function PaymentMethods() {
                 dayReportData.data.map((val, index) => {
                     const targetPercentage = Math.round(val.total * 100 / dayTotal)
                     return (
-                        <Row key={'paymentmethodtoday' + index}>
+                        <Row key={'paymentmethodtoday' + index} onClick={() => openDetails(val["payment mode"])}>
                             <Col lg={4} xs={5}>{getPaymentMethodName(val["payment mode"])}</Col>
-                            <Col xs={7} className="d-lg-none text-end align-bottom">{currencyFormatter.format(val.total)} ({targetPercentage}%)</Col>
+                            <Col xs={7} className="d-lg-none text-end align-bottom">{currencyFormatter.format(val.total)} <span className="small text-color-50">({val.count} payments)</span></Col>
                             <Col lg={4} className="mt-2">
                                 <OverlayTrigger overlay={
                                     <Tooltip id="tooltip-disabled">{targetPercentage}% Achieved</Tooltip>}>
