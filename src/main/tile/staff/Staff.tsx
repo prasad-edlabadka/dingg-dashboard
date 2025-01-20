0aad4e081203a444e086133021011c3b685b8f6f
Prasad Edlabadkar <prasad.edlabadkar@outlook.com>
2025-01-20 19:44:07 +0530

    updated staff targets

--- src/main/tile/staff/Staff.tsx
+++ src/main/tile/staff/Staff.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useCallback } from "react";
 import { Col, Row } from "react-bootstrap";
 import { currencyFormatter, formatDate, getLastMonth } from "../Utility";
 import { TokenContext, API_BASE_URL } from "../../../App";
@@ -8,100 +8,137 @@ import DiwaRefreshButton from "../../../components/button/DiwaRefreshButton";
 import TargetProgress from "./TargetProgress";
 import { addMonths } from "date-fns";
 
+interface EmployeeData {
+  id: number;
+  name: string;
+  target: number;
+  dinggCalculatedTarget: number;
+  withoutDiscount: number;
+  withDiscount: number;
+  tip: number;
+  membership: number;
+  membershipTotal: number;
+  completionPercentage: number;
+}
+
 export default function Staff() {
-  const { callAPI } = useContext(TokenContext);
-  const [reportData, setReportData] = useState({ data: [{ "service price": 0, "service amount": 0, stylist: "" }] });
-  const [membershipsSold, setMembershipsSold] = useState([{ "membership count": 0, total: 0, stylist: "" }]);
+  const { callAPI, callAPIPromise } = useContext(TokenContext);
   const buttonState = useState(0);
   const [, setActiveButtonIndex] = buttonState;
   const [endDate, setEndDate] = useState(new Date());
   const [startDate, setStartDate] = useState(new Date(endDate.getFullYear(), endDate.getMonth(), 1));
   const [loading, setLoading] = useState(false);
 
-  const [staffTargets, setStaffTargets] = useState([
-    {
-      total_sales: "0",
-      employee: {
-        id: 0,
-        name: "",
-      },
-    },
-  ]);
-  const [tips, setTips] = useState([
-    {
-      "staff name": "",
-      "received tip": 0,
-      "unsettled tip": 0,
-      "settled tip": 0,
-    },
-  ]);
-  const [calculatedTarget, setCalculatedTarget] = useState<any>({});
+  const [employees, setEmployees] = useState<EmployeeData[]>([]);
 
   const defaultTarget = 100000;
 
   useEffect(() => {
+    const employeeData: EmployeeData[] = [];
     setLoading(true);
-    const apiURL = `${API_BASE_URL}/vendor/report/sales?start_date=${formatDate(
-      startDate
-    )}&report_type=staff_service_summary&end_date=${formatDate(endDate)}&app_type=web`;
-    callAPI(`${API_BASE_URL}/vendor/target/all`, (targetData: any) => {
-      setStaffTargets(getTargets(targetData));
-      callAPI(apiURL, (data: any) => {
-        data.data = data.data.sort((a: any, b: any) => {
-          return b["service price"] - a["service price"];
+    const employeeURL = `${API_BASE_URL}/employees/get`;
+    callAPI(employeeURL, (allEmployees: any) => {
+      allEmployees.data = allEmployees.data.filter(
+        (emp: any) => emp.title !== "Owner" && emp.title !== "Administrator"
+      );
+      Promise.all(
+        allEmployees.data.map((emp: any) => {
+          const targetURL = `${API_BASE_URL}/employee/setting?employee_id=${emp.id}`;
+          return callAPIPromise(targetURL).then((targetData: any) => {
+            return {
+              id: emp.id,
+              name: emp.name,
+              target: targetData.data?.sales_target || defaultTarget,
+            };
+          });
+        })
+      ).then(async (data: EmployeeData[]) => {
+        setEmployees(data);
+        const empList = data.map((v) => v.id).join(",");
+        const dinggCalculatedAPIURL = `${API_BASE_URL}/vendor/target/all?employee_ids=${empList}&time_type=monthly&start_date=${formatDate(
+          startDate
+        )}&end_date=${formatDate(endDate)}`;
+
+        //Dingg Calculated target
+        const reportData = await callAPIPromise(dinggCalculatedAPIURL);
+        data = data.map((v) => {
+          const target = reportData.data.find((r: any) => r.employee.id === v.id)?.service_sales_achieved || 0;
+          const targetPercentage = Math.round((target * 100) / v.target);
+          return { ...v, dinggCalculatedTarget: Number.parseFloat(target), completionPercentage: targetPercentage };
         });
-        setReportData(data);
+
+        //Salon calculated target
+        const salonAPIURL = `${API_BASE_URL}/vendor/report/sales?start_date=${formatDate(
+          startDate
+        )}&report_type=staff_service_summary&end_date=${formatDate(endDate)}&app_type=web`;
+        const salonReportData = await callAPIPromise(salonAPIURL);
+        data = data.map((v) => {
+          const emp = salonReportData.data.find((r: any) => r.stylist === v.name) || {};
+          const withDiscount = emp["service amount"] || 0;
+          const withoutDiscount = emp["service price"] || 0;
+          return { ...v, withoutDiscount: withoutDiscount, withDiscount: withDiscount };
+        });
+
+        //Tip
+        const tipURL = `${API_BASE_URL}/vendor/report/sales?report_type=by_staff_tip_summary&start_date=${formatDate(
+          startDate
+        )}&end_date=${formatDate(endDate)}&app_type=web`;
+        const tipData = await callAPIPromise(tipURL);
+        data = data.map((v) => {
+          const tip = tipData.data.find((r: any) => r["staff name"] === v.name) || {};
+          return { ...v, tip: tip["received tip"] || 0 };
+        });
+
+        //Membership
+        const membershipSoldURL = `${API_BASE_URL}/vendor/report/sales?report_type=staff_by_membership&start_date=${formatDate(
+          startDate
+        )}&end_date=${formatDate(endDate)}&app_type=web&range_type=month`;
+        const membershipData = await callAPIPromise(membershipSoldURL);
+        data = data.map((v) => {
+          const membership = membershipData.data.find((r: any) => r.stylist === v.name) || {};
+          return {
+            ...v,
+            membership: Number.parseFloat(membership["membership count"] || 0),
+            membershipTotal: membership.total || 0,
+          };
+        });
+
+        //Update manager's data
+        data = data.map((v) => {
+          if (v.name === "Manager") {
+            const dinggCalculatedTarget = data.reduce((acc, val) => acc + val.dinggCalculatedTarget, 0);
+            const withoutDiscount = data.reduce((acc, val) => acc + val.withoutDiscount, 0);
+            const withDiscount = data.reduce((acc, val) => acc + val.withDiscount, 0);
+            const tip = data.reduce((acc, val) => acc + val.tip, 0);
+            const membership = data.reduce((acc, val) => acc + val.membership, 0);
+            const membershipTotal = data.reduce((acc, val) => acc + val.membershipTotal, 0);
+            return {
+              ...v,
+              dinggCalculatedTarget: dinggCalculatedTarget,
+              withoutDiscount: withoutDiscount,
+              withDiscount: withDiscount,
+              tip: tip,
+              membership: membership,
+              membershipTotal: membershipTotal,
+            };
+          }
+          return v;
+        });
+
+        data = data.sort((a: EmployeeData, b: EmployeeData) => {
+          return a.completionPercentage >= b.completionPercentage ? -1 : 1;
+        });
+
+        setEmployees(data);
+        console.log(data);
         setLoading(false);
       });
     });
-
-    const employeeURL = `${API_BASE_URL}/employees/get`;
-    callAPI(employeeURL, (data: any) => {
-      const empList = data.data.map((v: { id: any }) => v.id).join(",");
-      const reportApiURL = `${API_BASE_URL}/vendor/target/all?employee_ids=${empList}&time_type=monthly&start_date=${formatDate(
-        startDate
-      )}&end_date=${formatDate(endDate)}`;
-      callAPI(reportApiURL, (reportData: any) => {
-        const calculatedTarget: any = {};
-        reportData.data.forEach((v: { employee: { name: string }; service_sales_achieved: number }) => {
-          calculatedTarget[v.employee.name] = v.service_sales_achieved;
-        });
-        console.log(calculatedTarget);
-        setCalculatedTarget(calculatedTarget);
-      });
-    });
-
-    const tipURL = `${API_BASE_URL}/vendor/report/sales?report_type=by_staff_tip_summary&start_date=${formatDate(
-      startDate
-    )}&end_date=${formatDate(endDate)}&app_type=web`;
-    callAPI(tipURL, (data: any) => {
-      setTips(data.data);
-    });
-
-    //https://api.dingg.app/api/v1/vendor/report/sales?start_date=2024-11-01&report_type=staff_by_membership&end_date=2024-11-30&locations=null&app_type=web&range_type=month
-    const membershipSoldURL = `${API_BASE_URL}/vendor/report/sales?report_type=staff_by_membership&start_date=${formatDate(
-      startDate
-    )}&end_date=${formatDate(endDate)}&app_type=web&range_type=month`;
-    callAPI(membershipSoldURL, (data: any) => {
-      setMembershipsSold(data.data);
-    });
-
-    const getTargets = (targetData: any) => {
-      return targetData.data;
-    };
-  }, [startDate, endDate, callAPI]);
+  }, [callAPI, startDate, endDate]);
 
   const refresh = () => {
     setDuration("current");
     setActiveButtonIndex(0);
-  };
-
-  const getTarget = (name: string) => {
-    if (!staffTargets) return defaultTarget;
-    return Number.parseFloat(
-      staffTargets.find((v) => v?.employee?.name.toLowerCase() === name.toLowerCase())?.total_sales ||
-        `${defaultTarget}`
-    );
   };
 
   const date = new Date();
@@ -137,9 +174,9 @@ export default function Staff() {
     },
   ];
 
-  const calculatePercentage = (achieved: number, target: number) => {
+  const calculatePercentage = useCallback((achieved: number, target: number) => {
     return Math.round((achieved * 100) / target);
-  };
+  }, []);
 
   return (
     <DiwaCard varient="indigo" loadingTracker={loading}>
@@ -150,57 +187,49 @@ export default function Staff() {
         <DiwaRefreshButton refresh={() => refresh()} />
       </div>
       <DiwaButtonGroup buttons={buttons} state={buttonState} />
+
       <Row className="gx-3">
-        {reportData.data.map((val, index) => {
-          const target = getTarget(val.stylist.trim());
-          const targetPercentage = Math.round((val["service price"] * 100) / target);
-          const targetNoDiscountPercentage = Math.round((val["service amount"] * 100) / target);
-          const tip = tips?.find(
-            (v: { "staff name": string }) => v["staff name"].toLowerCase() === val.stylist.toLowerCase()
-          );
-          const membership = membershipsSold?.find((v) => v.stylist.toLowerCase() === val.stylist.toLowerCase());
+        {employees.map((val, index) => {
           return (
             <Col xs={12} md={4} key={"staff" + index} className="">
               <div className="mt-3 pt-2 pb-2 rounded black-bg">
                 <Row className="ps-2 pe-2 align-bottom">
                   <Col xs={7} className="align-bottom pe-0 text-color">
-                    <h4>{val.stylist}</h4>
+                    <h4>{val.name}</h4>
                   </Col>
                   <Col xs={5} className="text-end align-bottom text-color-50 ps-0">
-                    Target {currencyFormatter.format(target)}
+                    Target {currencyFormatter.format(val.target)}
                   </Col>
                 </Row>
                 <TargetProgress
                   label="Without discount"
-                  value={val["service price"]}
-                  target={target}
-                  percentAchieved={targetPercentage}
+                  value={val.withoutDiscount}
+                  target={val.target}
+                  percentAchieved={calculatePercentage(val.withoutDiscount, val.target)}
                 />
                 <TargetProgress
                   label="With discount"
-                  value={val["service amount"]}
-                  target={target}
-                  percentAchieved={targetNoDiscountPercentage}
+                  value={val.withDiscount}
+                  target={val.target}
+                  percentAchieved={calculatePercentage(val.withDiscount, val.target)}
                 />
                 <TargetProgress
                   label="Dingg Calculated"
-                  value={calculatedTarget[val.stylist]}
-                  target={target}
-                  percentAchieved={calculatePercentage(calculatedTarget[val.stylist], target)}
+                  value={val.dinggCalculatedTarget}
+                  target={val.target}
+                  percentAchieved={calculatePercentage(val.dinggCalculatedTarget, val.target)}
                 />
                 <TargetProgress
                   label="Tip Received"
-                  value={tip?.["received tip"] || 0}
-                  target={tip?.["received tip"] || 0}
-                  percentAchieved={calculatePercentage(tip?.["settled tip"] || 0, tip?.["received tip"] || 0)}
+                  value={val.tip}
+                  target={val.tip}
+                  percentAchieved={100}
                   percentAchievedSuffix="Paid"
                 />
                 <TargetProgress
                   label="Memberships Sold"
-                  value={`${membership?.["membership count"] || 0} (${currencyFormatter.format(
-                    membership?.total || 0
-                  )})`}
-                  target={membership?.["membership count"] || 0}
+                  value={`${val.membership} (${currencyFormatter.format(val.membershipTotal)})`}
+                  target={val.membership}
                   percentAchieved={100}
                   type="number"
                 />
